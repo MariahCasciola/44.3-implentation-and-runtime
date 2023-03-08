f1a1549b6c3e95a8a181e68a1a41b1ff81774371
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-08 10:41:43 -0600

    insert function is complete and passing tests

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -7,8 +7,29 @@ class BinarySearchTree {
     this.right = null;
   }
 
-  insert() {
-    // your solution here
+  insert(key, value) {
+    // if the tree is empty this will insert the root node of the tree
+    if (this.key == null) {
+      this.key = key;
+      this.value = value;
+    }
+    // compare key you want to insert to the root key if a tree already exists
+    else if (key < this.key) {
+      // checking if left child is empty
+      if (this.left == null) {
+        //
+        this.left = new BinarySearchTree(key, value, this);
+      } //recursively call insert, if node has an existing left child, so the node is added further down the tree
+      else {
+        this.left.insert(key, value);
+      }
+    } else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
   }
 
   find() {
